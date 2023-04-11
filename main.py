30b99c5bd77b175cc4742db5b9021718c12e561d
GreenEngine <99599566+GreenEngine@users.noreply.github.com>
2023-04-11 04:58:19 +0300

    deleted observer

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 
 import requests
 from PIL import Image
-import sys
 import logging
 import commands
-from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext , Application,ContextTypes
 import sqlite3
@@ -227,18 +225,4 @@ def main() -> None:
     print('start')
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
-    observer.start()
-    try:
-        while observer.is_alive():
-            main()
-            observer.join(1)
-    finally:
-        observer.stop()
-        observer.join()
+    main()
